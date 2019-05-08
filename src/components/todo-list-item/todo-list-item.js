3d1ea9b1fcddcf6be5b00b1ee0864e5effff008b
Artur Ratynsky <aratinskii@gmail.com>
2019-05-08 16:47:13 +0300

    add event

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -22,8 +22,11 @@ export default class TodoListItem extends Component {
       }
     });
   };
+  onDeleted = () => {
+
+  }
   render() {
-    const {label} = this.props;
+    const {label, onDeleted} = this.props;
     const {done, important} = this.state;
     let classNames="todo-list-item";
     if (done) {
@@ -48,7 +51,8 @@ export default class TodoListItem extends Component {
         </button>
   
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
           <i className="fa fa-trash-o" />
         </button>
       </span>
